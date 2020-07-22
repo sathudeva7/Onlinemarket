d2a13568e49c77861a6ab497750d64e44085e90c
devasathu7 <67930416+sathudeva7@users.noreply.github.com>
2020-07-22 17:44:36 +0530

    hello

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,14 @@ const __dirname = path.resolve();
  dotenv.config();
 
 const mongodbUrl = config.MONGODB_URL;
-mongoose.connect(mongodbUrl, {
+mongoose.connect( process.env.MONGODB_URL ||mongodbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 }).catch(error => console.log(error.reason));
 
 const app =express();
-
+const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use("/api/uploads",uploadRoute)
 
@@ -46,8 +46,12 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
   //  res.send(data.products);
 //});
 
-app.listen(5000, () => {
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static("/frontend/build"))
+}
+
+app.listen(PORT, () => {
   console.log("Server started at http://localhost:5000")
-  console.log(__dirname)
+
 
 });
